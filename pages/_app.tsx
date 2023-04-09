67a6e3c3e1a0ae98cd4ac9792c9f32d01065b956
Sydney Shugarman <srs238@miami.edu>
2023-04-09 13:25:58 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,28 @@ import SalimovHead from "../src/SalimovHead";
 import "../styles/globals.css";
 import { Fragment } from "react";
 
-const App = ({ Component, pageProps }) => {
-  return (
-    <Fragment>
-      <SalimovHead />
-      <Switcher />
-      <Preloader />
-      <Component {...pageProps} />
-    </Fragment>
-  );
-};
-export default App;
+// const App = ({ Component, pageProps }) => {
+//   return (
+//     <Fragment>
+//       <SalimovHead />
+//       <Switcher />
+//       <Preloader />
+//       <Component {...pageProps} />
+//     </Fragment>
+//   );
+// };
+// export default App;
+
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+  <Fragment>
+    <SalimovHead />
+    <Switcher />
+    <Preloader />
+    <Component {...pageProps} />
+  </Fragment>
+}
+
+export default MyApp
